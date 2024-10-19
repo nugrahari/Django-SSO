093012f5623e85f424f4c2b0f412ad775cb2e33e
nugrahari <harin260@gmail.com>
2024-10-19 14:48:58 +0700

    Add penjelasan singkat

--- Service1/apps/grpc/management/commands/run_grpc_server.py
+++ Service1/apps/grpc/management/commands/run_grpc_server.py
@@ -1,9 +1,16 @@
+import grpc
+from concurrent import futures
 from django.core.management.base import BaseCommand
-from apps.grpc.server import serve
+from apps.grpc.server import serve as grpc_serve  # import grpc server function
 
 
 class Command(BaseCommand):
     help = 'Run the gRPC server'
 
     def handle(self, *args, **kwargs):
-        serve()
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        grpc_serve(server)  # Register your gRPC services here
+        server.add_insecure_port('[::]:50051')
+        server.start()
+        print("gRPC server is running on port 50051...")
+        server.wait_for_termination()
